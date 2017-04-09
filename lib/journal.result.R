c3fd7b0c973e49fbed83ab44451e2eaf9ea8c7ed
BoyaZhao <zhaoby1994@gmail.com>
2017-04-09 17:33:25 -0400

    z

--- lib/journal.result.R
+++ lib/journal.result.R
@@ -8,5 +8,11 @@ library("tm")
 data.lib <- "~/Desktop/sem 2/Applied data science/Spr2017-proj4-team-14/data/nameset"
 data.files <- list.files(path = data.lib, "*.txt")
 AKumar <- read.data(data.files[2])
+# split data
+index <- split_data(AKumar)
+train <- AKumar[index, ]
+test <- AKumar[-index, ]
 # train result
-ak <- p.journal(AKumar)
+ak.train <- p.journal(AKumar)
+ak.test <- p.journal(test)
+ak <- p.journal(AKumar, AKumar)
--- lib/p.function.R
+++ lib/p.function.R
@@ -24,16 +24,16 @@ word.infor <- function(df) {
   dic <- tm_map(dic, content_transformer(tolower))
   dic <- tm_map(dic, removePunctuation)
   dic <- tm_map(dic, removeWords, stopwords("english"))
-  dic <- lapply(dic, s)
+  dic <- lapply(dic, dec)
   dic <- unlist(dic)
   dic <- sort(table(dic), decreasing = T)
-  dic <- dic[-1]
+  if ("" %in% names(dic)) { dic <- dic[-1]}
   return(dic)
 }
 
 # find word location in the total dictionary and compute the probability of each word
 find_loc <- function(input) {
-  output <- rep(0, I)
+  output <- rep(0, L)
   for (i in 1:length(input)) {
     index <- which(dic == names(input)[i])
     output[index] <- input[i] / sum(input)
@@ -49,36 +49,36 @@ prior_p <- function(j, df) {
   # probability of the author publish a paper on a journal with a seen word in the journal title
   p.title.seen <- sum(subdic[subdic >= 2]) / sum(subdic)
   # probability of the author publish a paper on a journal with a unseen word in the journal title
-  p.title.unseen <- 1 - p.seen
+  p.title.unseen <- 1 - p.title.seen
   # probability of the author publish a paper on a journal with kth word condition on the jornal name has a seen word
   p.word.seen <- find_loc(subdic)
   # probability of the author publish a paper on a journal with kth word condition on the jornal name has a unseen word
-  p.word.unseen <- 1 / (all)
+  p.word.unseen <- 1 / (n.all)
   output <- c(p.title.seen, p.title.unseen, p.word.seen, p.word.unseen)
   return(output)
 }
 
 # split data
 split_data <- function(df){
-  #input: m = one class of one nameset
-  #output: train and test dataset
-  obs <- 1:nrow(df)
-  index <- sample(1:nrow(df), round(nrow(df)/2))
-  train_data <- obs[index]
-  test_data <- obs[-index]
-  output <- list(train_data = train_data, test_data = test_data)
+  author <- unique(df$clusterid)
+  output <- numeric(0)
+  for(i in 1:length(author)) {
+    obs <- which(df$clusterid == author[i])
+    random <- sample(obs, round(length(obs)/2))
+    output <- c(output, random)
+  }
   return(output)
 }
 
 # train function
-p.journal <- function(df) {
+p.journal <- function(df, total_df) {
   # the total word dictionary
-  dic <- word.infor(df)
-  all <- sum(dic)
+  dic <<- word.infor(total_df)
+  n.all <<- sum(dic)
   dic <- names(dic)
   author <- sort(unique(df$clusterid))
   K <- length(author)
-  I <- length(dic)
+  L <<- length(dic)
   q <- matrix(1:K, ncol = 1)
   prior.dic <- apply(q, 1, prior_p, df)
   prior.author <- table(df$clusterid) / length(df$clusterid)
@@ -87,3 +87,4 @@ p.journal <- function(df) {
   row.names(output) <- c("p.title.seen", "p.title.unseen", dic, "p.word.unseen", "prior.author")
   return(output)
 }
+
