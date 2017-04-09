fb4324ba7de7097ff7f05aaa7e2be2fdc44ced06
nw2359 <nw2359@columbia.edu>
2017-04-09 16:29:03 -0400

    experiments and read_files function upload

---
+++ lib/wang_experiments.R
@@ -0,0 +1,19 @@
+###Ten Experiments Function#########
+
+experiment <- function(train_files, test_files){
+  
+  #input: 10 random sampled train sets and test sets of one nameset
+  #output: the mean and standard deviation of accuracy
+  
+  model <- NULL
+  prediction <- NULL
+  accuracy <- NA
+  for(i in 1:10){
+    train_x <- as.matrix(train_files[[i]][,-1])
+    train_y <- train_files[[i]][,1]
+    model[[i]] <- nb_train(train_x, train_y)
+    prediction[[i]] <- nb_test(model[[i]], test_files[[i]][,-1])
+    accuracy[i] <- mean(prediction[[i]] == test_files[[i]][,1])
+  }
+  return(mean = mean(accuracy), StdDev = sd(accuracy))
+}
---
+++ lib/wang_read_files.R
@@ -0,0 +1,23 @@
+###read 10 experiments data sets###
+
+read_files <- function(num_nameset){
+  
+  #input: which nameset to read (indicated by number)
+  #output: 10 experiments train and test data set for that nameset
+  
+  trainset <- paste("/train_", num_nameset, ".csv", sep = "")
+  testser <- paste("/test_", num_nameset, ".csv", sep = "")
+  
+  train_files <- NULL
+  for(i in 1:10){
+    file_dir <- paste("/Users/ouminamikun/Documents/Columbia/Spring 2017/ADS/Spr2017-proj4-team-14/output/Exp",i,trainset,sep = "")
+    train_files[[i]] <- read.csv(file_dir, header = T)
+  }
+  
+  test_files <- NULL
+  for(i in 1:10){
+    file_dir <- paste("/Users/ouminamikun/Documents/Columbia/Spring 2017/ADS/Spr2017-proj4-team-14/output/Exp",i,trainset,sep = "")
+    test_files[[i]] <- read.csv(file_dir, header = T)
+  }
+  return(list(train_files = train_files,test_files = test_files))
+}
