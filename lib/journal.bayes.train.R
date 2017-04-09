307594b71f1ac5b6964faf3f810c95f8c6648edb
BoyaZhao <zhaoby1994@gmail.com>
2017-04-09 15:42:21 -0400

    z

---
+++ lib/journal.bayes.train.R
@@ -0,0 +1,91 @@
+setwd("~/Desktop/sem 2/Applied data science/Spr2017-proj4-team-14/data/nameset")
+
+# Needed <- c("tm", "NLP", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
+# install.packages(Needed, dependencies=TRUE)
+# install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/", type = "source")
+
+library("NLP")
+library("tm")
+
+
+AKumar <- data.frame(scan("AKumar.txt",
+                          what = list(Coauthor = "", Paper = "", Journal = ""),
+                          sep=">", quiet=TRUE), stringsAsFactors=F)
+AKumar$clusterid <- sub("_.*","",AKumar$Coauthor) # author_id
+AKumar$citationid <- sub(".*_(\\w*)\\s.*", "\\1", AKumar$Coauthor) # paper_id
+AKumar$coauthor <- gsub("<","",sub("^.*?\\s","", AKumar$Coauthor))
+AKumar$Paper <- gsub("<","",AKumar$Paper)
+
+
+attach("CleanData.RData")
+data.files <- list.files(path = data.lib, "*.txt")
+query.list <- substring(data.files, 1, nchar(data.files)-4)
+names(data_list) <- query.list
+
+s <- function(z) { return(strsplit(z, split = " ")) }
+
+# read in Journal name
+#a <- paste(AKumar$Journal, collapse = " ")
+# a = tolower(a) #make it lower case
+# a = gsub('[[:punct:]]', '', a)
+# a <- strsplit(a, split = " ")
+# n <- length(a[[1]])
+# n
+
+dic <- Corpus(VectorSource(AKumar$Journal))
+dic <- tm_map(dic, content_transformer(tolower))
+dic <- tm_map(dic, removePunctuation)
+dic <- tm_map(dic, removeWords, stopwords("english"))
+dic <- lapply(dic, s)
+dic <- unlist(dic)
+dic <- sort(table(dic), decreasing = T)
+dic <- dic[-1]
+all <- sum(dic)
+dic <- names(dic)
+
+author <- sort(unique(AKumar$clusterid))
+K <- length(author)
+
+I <- length(dic)
+J <- length(author)
+
+
+find_loc <- function(input) {
+  output <- rep(0, I)
+  for (i in 1:length(input)) {
+    index <- which(dic == names(input)[i])
+    output[index] <- input[i] / sum(input)
+  }
+  return(output)
+}
+
+# prior probability to each author in dataset
+####### should put variable all in function def?
+prior_p <- function(j, df) {
+  author <- sort(unique(df$clusterid))
+  subdata <- subset(df, clusterid == author[j])
+  subdic <- Corpus(VectorSource(subdata$Journal))
+  subdic <- tm_map(subdic, content_transformer(tolower))
+  subdic <- tm_map(subdic, removePunctuation)
+  subdic <- tm_map(subdic, removeWords, stopwords("english"))
+  subdic <- lapply(subdic, s)
+  subdic <- unlist(subdic)
+  subdic <- sort(table(subdic), decreasing = T)
+  subdic <- subdic[-1]
+  # probability of the author publish a paper on a journal with a seen word in the journal title
+  p.title.seen <- sum(subdic[subdic >= 2]) / sum(subdic)
+  # probability of the author publish a paper on a journal with a unseen word in the journal title
+  p.title.unseen <- 1 - p.seen
+  # probability of the author publish a paper on a journal with kth word condition on the jornal name has a seen word
+  p.word.seen <- find_loc(subdic)
+  # probability of the author publish a paper on a journal with kth word condition on the jornal name has a unseen word
+  p.word.unseen <- 1 / (all)
+  output <- c(p.title.seen, p.title.unseen, p.word.seen, p.word.unseen)
+  return(output)
+}
+
+q <- matrix(1:J, ncol = 1)
+prior.dic <- apply(q, 1, prior_p, AKumar)
+colnames(prior.dic) <- author
+row.names(prior.dic) <- c("p.title.seen", "p.title.unseen", dic, "p.word.unseen")
+prior.author <- table(AKumar$clusterid) / length(AKumar$clusterid)
